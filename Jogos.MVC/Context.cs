d62b34e66154e4e29abc24642bdda0162f6b4736
Walter Nogueira <netoneto796@gmail.com>
2021-09-25 00:43:17 -0300

    Adicionar arquivos de projeto.

---
+++ Jogos.MVC/Context.cs
@@ -0,0 +1,17 @@
+﻿using Jogos.MVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jogos.MVC
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+        : base(options) { }
+        public DbSet <Jogo> Jogos { get; set; }
+
+    }
+}
---
+++ Jogos.MVC/Models/Jogo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jogos.MVC.Models
+{
+    public class Jogo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
